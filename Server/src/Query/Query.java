3777b8a3da95273a340ac558f113779085d7b08d
heyaayush.py@gmail.com <heyaayush.py@gmail.com>
2020-11-01 10:53:16 +0530

    Merged

--- Server/src/Query/Query.java
+++
@@ -1,9 +0,0 @@
-package Query;
-
-import EnumConstants.QueryCode;
-
-import java.io.Serializable;
-
-public abstract class Query implements Serializable {
-        public abstract QueryCode getQueryCode();
-}
--- Server/src/Streamer/StreamingConstants.java
+++
@@ -1,7 +0,0 @@
-package Streamer;
-
-import java.io.Serializable;
-
-public enum StreamingConstants implements Serializable {
-    REQUEST_STREAMING_ROOM, REQUEST_STOP_STREAMING, REQUEST_JOIN_GROUP, FOR_STREAMING, FOR_JOINING;
-}
